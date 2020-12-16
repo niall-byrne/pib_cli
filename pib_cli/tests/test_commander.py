7a3e58c1d7fa079f51bdd0a8bcf772cf3011283a
Niall Byrne <niall@niallbyrne.ca>
2020-12-16 22:43:15 +0000

    test(CLI.LINT): add tests for the lint command

--- pib_cli/tests/test_commander.py
+++ pib_cli/tests/test_commander.py
@@ -16,8 +16,22 @@ class TestSecTest(CommandTestHarness):
   __test__ = True
   invocation_command = ['sectest']
   expected_system_calls = [
-      'bandit -r "${PROJECT_NAME}" -c .bandit.rc --ini .bandit', 'safety check'
+      'bandit -r "${PROJECT_NAME}" -c .bandit.rc --ini .bandit',
+      'safety check',
   ]
   success_message = "Security Test Passes!"
   failure_message = "Security Test Failed!"
+  command_path_method = "project_root"
+
+
+class TestLint(CommandTestHarness):
+  __test__ = True
+  invocation_command = ['lint']
+  expected_system_calls = [
+      'isort -c "${PROJECT_NAME}"',
+      ('pytest --pylint --pylint-rcfile=.pylint.rc '
+       '--pylint-jobs=2 "${PROJECT_NAME}"'),
+  ]
+  success_message = "Lint Test Passes!"
+  failure_message = "Lint Test Failed!"
   command_path_method = "project_root"
