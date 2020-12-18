c852e9f570bd9894a3879672cda2c5f594ea2bb5
Niall Byrne <niall@niallbyrne.ca>
2020-12-18 18:56:51 +0000

    test(CLI): fix brittle tests, allow to run outside the container

--- pib_cli/tests/fixtures.py
+++ pib_cli/tests/fixtures.py
@@ -20,14 +20,18 @@ class CLITestHarness(TestCase):
     self.overload = self.__class__.overload
 
   @patch("pib_cli.cli.execute")
-  def test_command_invocation_no_overload(self, mock_execute):
+  @patch("pib_cli.support.paths.PathManager.is_container")
+  def test_command_invocation_no_overload(self, mock_container, mock_execute):
     if self.overload is None:
+      mock_container.return_value = True
       self.runner.invoke(cli, self.invocation_command)
       mock_execute.assert_called_once_with(self.internal_commands)
 
   @patch("pib_cli.cli.execute")
-  def test_command_invocation_with_overload(self, mock_execute):
+  @patch("pib_cli.support.paths.PathManager.is_container")
+  def test_command_invocation_with_overload(self, mock_container, mock_execute):
     if self.overload is not None:
+      mock_container.return_value = True
       self.runner.invoke(cli, self.invocation_command + list(self.overload))
       mock_execute.assert_called_once_with(
           self.internal_commands,
