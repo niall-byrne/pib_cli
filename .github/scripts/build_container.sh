497cfcdafce1ce0a149c682d0f49c9c928512cc6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-02-15 15:39:18 +0000

    build(DOCKER): update dockerfile to latest pib

--- .github/scripts/build_container.sh
+++ .github/scripts/build_container.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 main() {
 
-  docker-compose build --build-arg PYTHON_VERSION="${PYTHON_VERSION}"
+  docker-compose build --build-arg BUILD_ARG_PYTHON_VERSION="${PYTHON_VERSION}"
   docker-compose up -d
 
 }
