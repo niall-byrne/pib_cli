251c4e08cfc39b618aece6bd4aae733a9b046fb6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-02-26 16:48:50 +0000

    fix(CI): ensure tags are sorted correctly

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 main() {
 
   echo "CHANGE_LOG<<EOF" >> "${GITHUB_ENV}"
-  cz ch --start-rev "$(git tag | tail -n2 | head -n1)" --dry-run >> "$GITHUB_ENV"
+  cz ch --start-rev "$(git tag | sort -V | tail -n2 | head -n1)" --dry-run >> "$GITHUB_ENV"
   echo "EOF" >> "${GITHUB_ENV}"
 
 }
