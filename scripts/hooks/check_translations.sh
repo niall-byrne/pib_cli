e3b77a9038bc82d33f98354f83c525d31d60dd4d
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-03-15 18:30:54 +0000

    ci(GITHUB): add check for translation content

--- scripts/hooks/check_translations.sh
+++ scripts/hooks/check_translations.sh
@@ -11,6 +11,11 @@ translation_check() {
 
   dev babel-extract > /dev/null 2>&1
 
+  pushd documentation > /dev/null 2>&1 || exit 127
+    make gettext > /dev/null 2>&1
+    sphinx-intl update -p build/gettext -l en > /dev/null 2>&1
+  popd > /dev/null 2>&1
+
   if ! git diff --exit-code pib_cli/config/locale/en/LC_MESSAGES/base.po; then
     echo -e "\\nWARNING: there has been a change to the translation content detected!\\n"
     exit 127
