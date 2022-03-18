d3dced08615b65294c0cb7138d7585f7870c0dc8
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-03-18 18:44:47 +0000

    feat(SUPPORT.PATH_MAP): user friendly error msg

--- pib_cli/support/path_map.py
+++ pib_cli/support/path_map.py
@@ -1,19 +1,19 @@
 """PathMap class."""
 
 import os
+import pathlib
+import sys
 
+import click
 import git
-from pib_cli import config
+from pib_cli import _, config
 
 
 class PathMap:
   """Path mappings for a PIB git repository."""
 
   def __init__(self) -> None:
-    self.git_root_folder = git.Repo(
-        os.curdir,
-        search_parent_directories=True,
-    ).working_tree_dir
+    self.git_root_folder = self._get_repository_root()
     self.documentation_root_folder = os.path.join(
         self.git_root_folder,
         config.DOCUMENTATION_FOLDER_NAME,
@@ -25,6 +25,22 @@ class PathMap:
             "not-defined",
         ),
     )
+
+  def _get_repository_root(self) -> pathlib.Path:
+    try:
+      return self._get_repository_root_as_path()
+    except git.InvalidGitRepositoryError:
+      click.echo(_("No git repository not found."))
+      sys.exit(config.EXIT_CODE_NOT_A_REPOSITORY)
+
+  def _get_repository_root_as_path(self) -> pathlib.Path:
+    root = git.Repo(
+        os.curdir,
+        search_parent_directories=True,
+    ).working_tree_dir
+    if root:
+      return pathlib.Path(root).absolute()
+    return pathlib.Path(os.curdir).absolute()
 
   def documentation_root(self) -> None:
     """Change the path to the project documentation folder location."""
--- pib_cli/support/tests/test_path_map.py
+++ pib_cli/support/tests/test_path_map.py
@@ -1,10 +1,13 @@
 """Test the PathMap class."""
 
 import os
+from contextlib import ExitStack
 from pathlib import Path
+from typing import Optional, Union
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+import git
 from pib_cli import config
 from pib_cli.support import path_map
 
@@ -12,37 +15,74 @@ from pib_cli.support import path_map
 class TestPathMap(TestCase):
   """Test the PathMap class."""
 
+  instance: path_map.PathMap
+  mock_git_call: Mock
+
+  def create_instance(
+      self, working_tree_dir: Union[git.InvalidGitRepositoryError,
+                                    Optional[str]]
+  ) -> None:
+    with patch(path_map.__name__ + ".git.Repo") as m_git:
+      if isinstance(working_tree_dir, git.InvalidGitRepositoryError):
+        m_git.side_effect = working_tree_dir
+      else:
+        m_git.return_value.working_tree_dir = working_tree_dir
+      self.path_manager = path_map.PathMap()
+      self.mock_git_call = m_git
+
   @patch.dict(os.environ, {config.ENV_PROJECT_NAME: "test_name"}, clear=True)
   def setUp(self) -> None:
     self.mock_project_name = "test_name"
-    self.mock_git_root = Path("/app")
-    with patch(path_map.__name__ + ".git.Repo") as m_git:
-      m_git.return_value.working_tree_dir = Path("/app")
-      self.path_manager = path_map.PathMap()
-    self.mock_git_call = m_git
+    self.mock_git_root_str = "/app"
+    self.mock_git_root_path = Path(self.mock_git_root_str)
+    self.create_instance(working_tree_dir=self.mock_git_root_str)
 
-  def test_initialization_git_call(self) -> None:
+  def test_initialization_git_repo(self) -> None:
     self.mock_git_call.assert_called_once_with(
         '.',
         search_parent_directories=True,
     )
-
-  def test_initialization_instance_variables(self) -> None:
-    self.assertEqual(self.path_manager.git_root_folder, self.mock_git_root)
+    self.assertEqual(self.path_manager.git_root_folder, self.mock_git_root_path)
     self.assertEqual(
         self.path_manager.documentation_root_folder,
         os.path.join(
-            self.mock_git_root,
+            self.mock_git_root_path,
             config.DOCUMENTATION_FOLDER_NAME,
         ),
     )
     self.assertEqual(
         self.path_manager.project_root_folder,
         os.path.join(
-            self.mock_git_root,
+            self.mock_git_root_path,
             self.mock_project_name,
         ),
     )
+
+  def test_initialization_bare_git_repo(self) -> None:
+    self.create_instance(working_tree_dir=None)
+    self.mock_git_call.assert_called_once_with(
+        '.',
+        search_parent_directories=True,
+    )
+    self.assertEqual(
+        self.path_manager.git_root_folder,
+        Path(os.curdir).absolute()
+    )
+
+  def test_initialization_no_repo(self) -> None:
+    with ExitStack() as exit_stack:
+      exit_stack.enter_context(self.assertRaises(TypeError))
+      m_echo = exit_stack.enter_context(
+          patch(path_map.__name__ + ".click.echo")
+      )
+      m_exit = exit_stack.enter_context(patch(path_map.__name__ + ".sys.exit"))
+
+      self.create_instance(
+          working_tree_dir=git.
+          InvalidGitRepositoryError("Not a git repository.")
+      )
+      m_exit.assert_called_once_with(config.EXIT_CODE_NOT_A_REPOSITORY)
+      m_echo.assert_called_once_with("No git repository not found.")
 
   @patch(path_map.__name__ + ".os.chdir")
   def test_git_root(self, mock_chdir: Mock) -> None:
