3aba2089ddb30c9ecd8f0f8fdac7cc89687d5fc6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-03-18 15:26:35 +0000

    build(GIT): more thorough translation checks

--- scripts/hooks/check_translations.sh
+++ scripts/hooks/check_translations.sh
@@ -7,16 +7,25 @@
 
 set -eo pipefail
 
-translation_check() {
-
+check_python_translations() {
   dev babel-extract > /dev/null 2>&1
+}
 
+check_sphinx_translations() {
   pushd documentation > /dev/null 2>&1 || exit 127
     make gettext > /dev/null 2>&1
     sphinx-intl update -p build/gettext -l en > /dev/null 2>&1
   popd > /dev/null 2>&1
+}
 
-  if ! git diff --exit-code pib_cli/config/locale/en/LC_MESSAGES/base.po; then
+translation_check() {
+
+  check_python_translations &
+  check_sphinx_translations &
+
+  wait
+
+  if ! git diff --exit-code documentation/source/locale/en/LC_MESSAGES/*.po pib_cli/config/locale/en/LC_MESSAGES/base.po; then
     echo -e "\\nWARNING: there has been a change to the translation content detected!\\n"
     exit 127
   fi
