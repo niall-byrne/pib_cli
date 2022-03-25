0fa6bc1f41fd2d0855d6509a92c401a47935ba84
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2022-03-25 15:35:19 +0000

    refactor(ENVIRONMENT): rename project name env var

--- .github/scripts/release_pypi.sh
+++ .github/scripts/release_pypi.sh
@@ -7,7 +7,7 @@ main() {
   if [[ -n "${TEST_PYPI_API_TOKEN}" ]]; then
 
     echo "CD_TEST=true" >> "$GITHUB_ENV"
-    docker-compose exec -T "${PROJECT_NAME}" bash -c "                                                 \
+    docker-compose exec -T "${PIB_PROJECT_NAME}" bash -c "                                                 \
       poetry config repositories.testpypi https://test.pypi.org/legacy/                             && \
       poetry publish --build -r testpypi --username __token__ --password \"${TEST_PYPI_API_TOKEN}\"    \
     "
@@ -16,7 +16,7 @@ main() {
   if [[ -n "${PYPI_API_TOKEN}" ]]; then
 
     echo "CD_ENABLED=true" >> "$GITHUB_ENV"
-    docker-compose exec -T "${PROJECT_NAME}" bash -c "                                                 \
+    docker-compose exec -T "${PIB_PROJECT_NAME}" bash -c "                                                 \
       poetry publish --build --username __token__ --password \"${PYPI_API_TOKEN}\"                     \
     "
 
